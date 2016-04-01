f91ecfc3f2ef95fd11c15521c4c956450b228208
TheRealNOIG <Tyler.Gregorcyk@gmail.com>
2016-04-01 03:36:05 -0600

    Revert "Revert "First Push""
    This reverts commit 2120854a2a8535c7a3a0153af894170a3b6c0152.

---
+++ StoreLibrary/Item.cs
@@ -0,0 +1,21 @@
+﻿
+namespace StoreLibrary
+{
+    public class Item
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int AmountOfRequestsForItem { get; set; }
+        public Vendor Owner { get; set; }
+
+        public string DisplayNamePrice
+        {
+            get
+            {
+                return string.Format("{0} - {1}", Name, Price);
+            }
+        }
+    }
+}
